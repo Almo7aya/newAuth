67f8fe77a83c910c1e5ce8dd51c1205259cd499b
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-24 13:16:16 +0300

    add init user login

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,37 @@
 const router = require('express').Router();
+const {check, validationResult} = require('express-validator/check');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 router.get('/', (req, res, next) => {
-    res.end('login');
+    res.render('login', {
+        msg: '',
+        errors: [],
+        title: 'Login'
+    });
 });
 
+passport.use(new LocalStrategy());
+
+router.post('/', [
+        check('username').isLength({min: 1}).withMessage('Where is Username'),
+        check('password').isLength({min: 1}).withMessage('Where is password'),
+    ], passport.Authenticator('local' ,{
+        successRedirect: '/secret',
+        failureRedirect: '/login'
+    })
+    , (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.render('login', {
+                title: 'Login',
+                msg: 'errors',
+                errors: errors.array()
+            });
+        }
+
+    });
 
 module.exports = router;
