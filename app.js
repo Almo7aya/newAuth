7ed3db5d352acd681eb40321bcc8c995ea259054
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-24 11:14:08 +0300

    add everything

---
+++ app.js
@@ -0,0 +1,8 @@
+const exporess = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
