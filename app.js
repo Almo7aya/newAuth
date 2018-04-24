3ded7bfb11e3e2be0f073dba09905dbc78887123
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-24 11:28:14 +0300

    add init server files

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-const exporess = require('express');
+// main modules import
+const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const path = require('path');
+const morgan = require('morgan');
+const port = process.env.PORT || 8080;
 
+// init express app
+const app = express();
 
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// public files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false
+}));
+
+// set the routes
+app.use('', );
+app.use('login', );
+app.use('register', );
